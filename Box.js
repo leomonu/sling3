6e21f2bfe5f6e8fe91310dd369babcd60ce9cfc3
leomonu <62434366+leomonu@users.noreply.github.com>
2020-05-21 11:45:54 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,25 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+    this.image = loadImage("box.jpg");
+    this.visibility = 255;
+  }
+  display(){
+    //console.log(this.body.speed);
+    
+    if(this.body.speed<5){
+      super.display();
+    }
+    else{
+     
+      World.remove(world,this.body);
+      push();
+      this.visibility = this.visibility - 1000;
+      tint(255,this.visibility);
+      image(this.image,this.body.position.x,this.body.position.y,this.body.height,this.body.width);
+      pop();
+    }
+  }
+
+};
+
